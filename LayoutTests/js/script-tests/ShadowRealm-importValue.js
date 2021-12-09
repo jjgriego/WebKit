2ce46e70ec38c110363defbcae5fdd5f08aa69be
Joseph Griego <jgriego@igalia.com>
2021-12-09 17:01:32 -0500

    clean up importValue test

--- LayoutTests/js/script-tests/ShadowRealm-importValue.js
+++ LayoutTests/js/script-tests/ShadowRealm-importValue.js
@@ -1,10 +1,5 @@
 description("Test to ensure correct behaviour of ShadowRealm.prototype.importValue");
 
-function assert_closed_opener(w, closed, opener) {
-  assert_equals(w.closed, closed);
-  assert_equals(w.opener, opener);
-}
-
 function wrappedLog(prefix) {
     return function (msg) {
         debug(prefix + ": " + msg);
@@ -12,7 +7,6 @@ function wrappedLog(prefix) {
 }
 
 promise_test(async t => {
-  // const openee = window.open("", "greatname");
   const outerShadowRealm = new ShadowRealm();
   const checkFn = await outerShadowRealm.importValue("./example-module.js", "check");
   assert_equals(checkFn(wrappedLog("shadowRealm")), true);
@@ -32,7 +26,6 @@ promise_test(async t => {
 }, "can import module in a shadow realm");
 
 promise_test(async t => {
-    // const openee = window.open("", "greatname");
     const outerShadowRealm = new ShadowRealm();
     const checkFn = await outerShadowRealm.importValue("./example-module.js", "check_nested");
     assert_equals(checkFn(wrappedLog("shadowRealm")), true);
